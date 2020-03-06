3ec3aa0622b68df5b0abcb6bfa877074dc6d86d8
Alex White <5330681@gmail.com>
2020-03-06 17:49:05 +0300

    Main

---
+++ Snaike_OOP_Lesson/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snaike_OOP_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello World
+        }
+    }
+}
