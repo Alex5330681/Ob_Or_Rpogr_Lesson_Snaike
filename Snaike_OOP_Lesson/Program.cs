8d47c6c38ec7af3775006c5fb020422d23383507
Alex White <5330681@gmail.com>
2020-03-09 20:45:36 +0300

    Lesson-5

--- Snaike_OOP_Lesson/Program.cs
+++ Snaike_OOP_Lesson/Program.cs
@@ -10,16 +10,36 @@ namespace Snaike_OOP_Lesson
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(2, 4, '&'); 
+
+             Point p1 = new Point(2, 4, '&'); 
             p1.Draw();
 
             Point p2 = new Point(12, 10, '%'); 
             p2.Draw();
-
-
+        
 
             Console.ReadLine();
 
+
+            //Point p1 = new Point(1, 3, '*');
+            //Point p2 = new Point(4, 5, '#');
+
+            //Console.WriteLine($"p1 = {p1.x}, {p1.y}, {p1.sym}");
+            //Console.WriteLine($"p2 = {p2.x}, {p2.y}, {p2.sym}");
+
+            //p1 = p2;
+            //Console.WriteLine($"p1 = p2");
+
+            //Console.WriteLine($"p1 = {p1.x}, {p1.y}, {p1.sym}");
+            //Console.WriteLine($"p2 = {p2.x}, {p2.y}, {p2.sym}");
+
+            //p2.x++;
+            //p2.y++;
+            //Console.WriteLine($"p2++");
+            //Console.WriteLine($"p1 = {p1.x}, {p1.y}, {p1.sym}");
+            //Console.WriteLine($"p2 = {p2.x}, {p2.y}, {p2.sym}");
+
+            Console.ReadLine();
         }
     }
 }
